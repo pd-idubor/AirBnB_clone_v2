22220f47c52b74308c72b0cb6d7b4b7b1bf26766
Precious Idubor <preciousidubor@gmail.com>
2023-01-24 03:33:45 -0800

    Base change

--- models/base_model.py
+++ models/base_model.py
@@ -26,17 +26,6 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        """if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)"""
 
         if kwargs:
             for key, value in kwargs.items():
@@ -80,7 +69,7 @@ class BaseModel:
         cur_dict['__class__'] = str(type(self).__name__)
         cur_dict['created_at'] = self.created_at.isoformat()
         cur_dict['updated_at'] = self.updated_at.isoformat()
-        
+
         return cur_dict
 
     def delete(self):
