769922b5c47ce9e546f32ed55c127af58319bf21
Precious Idubor <preciousidubor@gmail.com>
2023-01-24 03:49:13 -0800

    Attr added

--- models/place.py
+++ models/place.py
@@ -15,9 +15,10 @@ class Place(BaseModel, Base):
         description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
         amenity_ids = []
 
     else:
