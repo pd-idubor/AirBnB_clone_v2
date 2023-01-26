74dcb707f2dbdf8f23256b563194f234e224da58
Precious Idubor <preciousidubor@gmail.com>
2023-01-26 15:14:24 -0800

    check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 #Script to set up web servers for deployment
 
-#Install nginx if not installed
-sudo apt-get update
-[ ! -f /usr/sbin/nginx ] && sudo apt-get install -y nginx
+sudo apt-get update -y
+[ ! -f /usr/sbin/nginx ] && sudo apt-get install nginx -y
 
 #Firewall
 sudo ufw allow 'Nginx HTTP'
 
+[ ! -d /data/web_static/releases/test/ ] && sudo mkdir -p /data/web_static/releases/test/
+[ ! -d /data/web_static/shared/ ] && sudo mkdir -p /data/web_static/shared/
 
-[ ! -d /data/web_static/shared/ ] && mkdir -p /data/web_static/shared/
-[ ! -d /data/web_static/releases/test/ ] && mkdir -p /data/web_static/releases/test/
+echo "<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<p>Configuration test<p>
+	</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-echo "Simple content to test Nginx configuration" > /data/web_static/releases/test/index.html
 
 [ -f /data/web_static/current ] && sudo rm /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R "ubuntu:ubuntu" /data/
 
-
-detail="alais \/data\/web\_static\/current\/;\n\t}\n"
-location="\n\tlocation \/hbnb\_static\/ {\n\t\t$detail"
+detail="alias \/data\/web\_static\/current\/;"
+location="\n\tlocation \/hbnb\_static\/ {\n\t\t$detail\n\t}\n"
 sed -i "37s/$/$location/" /etc/nginx/sites-available/default
 
 sudo service nginx restart
