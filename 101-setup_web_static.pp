6c66a488428631944e7b746b760bac33734df2e0
Precious Idubor <preciousidubor@gmail.com>
2023-01-26 18:27:13 -0800

    Deploy web static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,4 @@
-include stdlib
+# Configures a web server for deployment of web_static
 
 # Nginx configuration file
 $nginx_conf = "server {
@@ -24,52 +24,65 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-}
-->file { '/data':
+} ->
+
+file { '/data':
   ensure  => 'directory'
-}
-->file { '/data/web_static':
+} ->
+
+file { '/data/web_static':
   ensure => 'directory'
-}
-->file { '/data/web_static/releases':
+} ->
+
+file { '/data/web_static/releases':
   ensure => 'directory'
-}
-->file { '/data/web_static/releases/test':
+} ->
+
+file { '/data/web_static/releases/test':
   ensure => 'directory'
-}
-->file { '/data/web_static/shared':
+} ->
+
+file { '/data/web_static/shared':
   ensure => 'directory'
-}
-->file { '/data/web_static/releases/test/index.html':
+} ->
+
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Hey People\n"
-}
-->file { '/data/web_static/current':
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-}
-->exec { 'chown -R ubuntu:ubuntu /data/':
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
   ensure => 'directory'
-}
-->file { '/var/www/html':
+} ->
+
+file { '/var/www/html':
   ensure => 'directory'
-}
-->file { '/var/www/html/index.html':
+} ->
+
+file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Hello World!\n"
-}
-->file { '/var/www/html/404.html':
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-}
-->file { '/etc/nginx/sites-available/default':
+} ->
+
+file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-}
-->exec { 'nginx restart':
+} ->
+
+exec { 'nginx restart':
   path => '/etc/init.d/'
 }
