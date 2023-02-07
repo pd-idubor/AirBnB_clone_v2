f8e9e7c4213f95016823dc4f477624b2216a46d6
Precious Idubor <preciousidubor@gmail.com>
2023-02-06 20:26:05 -0800

    test2

--- models/review.py
+++ models/review.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ Model representation of `review` table
+    Attributes:
+        text: review
+        place_id: place id
+        user_id: user id
+    """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
--- models/user.py
+++ models/user.py
@@ -10,14 +10,18 @@ from models.review import Review
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-        places = relationship("Place", cascade="delete", backref='user')
-        reviews = relationship("Review", cascade="delete", backref='user')
+        places = relationship("Place",
+                              cascade="all, delete, delete-orphan",
+                              backref='user')
+        reviews = relationship("Review",
+                               cascade="all, delete, delete-orphan",
+                               backref='user')
 
     else:
         email = ''
